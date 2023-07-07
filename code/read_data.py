450233d378070c709ea0900d1a333298427f80bf
柯慕灵 <1985312383@qq.com>
2023-07-07 20:58:39 +0800

    Update .gitignore

---
+++ code/read_data.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import os
+# The behaviors.tsv file contains the impression logs and users' news click histories.
+# It has 5 columns divided by the tab symbol:
+# - Impression ID. The ID of an impression.
+# - User ID. The anonymous ID of a user.
+# - Time. The impression time with format "MM/DD/YYYY HH:MM:SS AM/PM".
+# - History. The news click history (ID list of clicked news) of this user before this impression.
+# - Impressions. List of news displayed in this impression and user's click behaviors on them (1 for click and 0 for non-click).
+behaviors_path = os.path.join(temp_dir, 'behaviors.tsv')
+pd.read_table(
+    behaviors_path,
+    header=None,
+    names=['impression_id', 'user_id', 'time', 'history', 'impressions'])
+
+# The news.tsv file contains the detailed information of news articles involved in the behaviors.tsv file.
+# It has 7 columns, which are divided by the tab symbol:
+# - News ID
+# - Category
+# - Subcategory
+# - Title
+# - Abstract
+# - URL
+# - Title Entities (entities contained in the title of this news)
+# - Abstract Entities (entities contained in the abstract of this news)
+news_path = os.path.join(temp_dir, 'news.tsv')
+pd.read_table(news_path,
+              header=None,
+              names=[
+                  'id', 'category', 'subcategory', 'title', 'abstract', 'url',
+                  'title_entities', 'abstract_entities'
+              ])
+
+# The entity_embedding.vec file contains the 100-dimensional embeddings
+# of the entities learned from the subgraph by TransE method.
+# The first column is the ID of entity, and the other columns are the embedding vector values.
+entity_embedding_path = os.path.join(temp_dir, 'entity_embedding.vec')
+entity_embedding = pd.read_table(entity_embedding_path, header=None)
+entity_embedding['vector'] = entity_embedding.iloc[:, 1:101].values.tolist()
+entity_embedding = entity_embedding[[0,
+                                     'vector']].rename(columns={0: "entity"})
+entity_embedding
+
+# The relation_embedding.vec file contains the 100-dimensional embeddings
+# of the relations learned from the subgraph by TransE method.
+# The first column is the ID of relation, and the other columns are the embedding vector values.
+relation_embedding_path = os.path.join(temp_dir, 'relation_embedding.vec')
+relation_embedding = pd.read_table(relation_embedding_path, header=None)
+relation_embedding['vector'] = relation_embedding.iloc[:,
+                                                       1:101].values.tolist()
+relation_embedding = relation_embedding[[0, 'vector'
+                                         ]].rename(columns={0: "relation"})
+relation_embedding
+
--- code/web/websever.py
+++ code/web/websever.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, escape, url_for
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return '{}\'s profile'.format(escape(username))
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
