8b1a677a0d1ff1dd346d6218acbdf0d90892f24e
柯慕灵 <1985312383@qq.com>
2023-07-10 15:23:51 +0800

    Update LR train

--- code/training/train.py
+++ code/training/train.py
@@ -1,19 +1,39 @@
-import pdb
 from read_data import read_data
 import pandas as pd
 import time
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, mean_squared_error, accuracy_score
 
-train_behaviors = read_data(mode='train', dataset='behaviors')
-train_behaviors['time1'] = train_behaviors['time'].apply(
-    lambda x: time.mktime(time.strptime(x, '%m/%d/%Y %H:%M:%S %p'))).astype('int64')
-train_behaviors['time2'] = train_behaviors['time'].str[-2:]
 
-# impressions = train_behaviors['impressions']
-train_behaviors = train_behaviors[['user_id', 'time1', 'time2', 'impressions']]
-train_behaviors['time_AM'] = train_behaviors['time2'].apply(lambda x: 1 if x == 'AM' else 0)
-train_behaviors['time_PM'] = train_behaviors['time2'].apply(lambda x: 1 if x == 'PM' else 0)
-train_behaviors = train_behaviors[['user_id', 'time1', 'time_AM', 'time_PM', 'impressions']]
-pdb.set_trace()
-train_behaviors = pd.get_dummies(train_behaviors)
-pdb.set_trace()
+def data_preprocess(mode):
+    behaviors = read_data(mode=mode, dataset='behaviors')
+    behaviors['time1'] = behaviors['time'].apply(
+        lambda x: time.mktime(time.strptime(x, '%m/%d/%Y %H:%M:%S %p'))).astype('int64')
+    behaviors['time2'] = behaviors['time'].str[-2:]
+
+    # impressions = train_behaviors['impressions']
+    behaviors = behaviors[['user_id', 'time1', 'time2', 'impressions']]
+    behaviors['user_id'] = behaviors['user_id'].map(lambda x: x.lstrip('U'))
+
+    behaviors['time_AM'] = behaviors['time2'].apply(lambda x: 1 if x == 'AM' else 0).astype('int32')
+    behaviors['time_PM'] = behaviors['time2'].apply(lambda x: 1 if x == 'PM' else 0).astype('int32')
+    behaviors = behaviors[['user_id', 'time1', 'time_AM', 'time_PM', 'impressions']]
+    behaviors = behaviors['impressions'].str.split(' ', expand=True).stack().reset_index(level=0).set_index(
+        'level_0').rename(columns={0: 'impressions'}).join(behaviors.drop('impressions', axis=1))
+    behaviors['impressions'] = behaviors['impressions'].map(lambda x: x.lstrip('N'))
+    if mode == 'train' or mode == 'val':
+        behaviors['impressions'], behaviors['lables'] = behaviors['impressions'].str.split('-').str
+        behaviors['lables'] = behaviors['lables'].astype('int32')
+    # train_behaviors = pd.get_dummies(train_behaviors)
+    return behaviors
+
+
+train_data = data_preprocess('train')
+lr = LogisticRegression()
+lr.fit(train_data.iloc[:, :-1], train_data.iloc[:, -1])
+
+val_data = data_preprocess('val')
+lr_pred = lr.predict(val_data.iloc[:, :-1])
+print("ROC", roc_auc_score(val_data.iloc[:, -1], lr_pred))
+print("acc", accuracy_score(val_data.iloc[:, -1], lr_pred))
+print("MSE", mean_squared_error(val_data.iloc[:, -1], lr_pred))
